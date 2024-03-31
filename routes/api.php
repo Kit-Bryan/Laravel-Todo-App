ef988646f86bb825282ec98eaf87fc9e5beb739a
KitBryan <bryanwwk@gmail.com>
2024-03-31 10:06:11 +0800

    added middleware authentication on all routes

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,8 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/', "App\\Http\\Controllers\\TasksController@index");
-Route::post("/tasks", "App\\Http\\Controllers\\TasksController@store");
-Route::patch("/complete", "App\\Http\\Controllers\\TasksController@update");
-Route::delete("/delete", "App\\Http\\Controllers\\TasksController@delete");
+Route::middleware(['auth:sanctum'])->get('/', "App\\Http\\Controllers\\TasksController@index");
+Route::middleware(['auth:sanctum'])->post("/tasks", "App\\Http\\Controllers\\TasksController@store");
+Route::middleware(['auth:sanctum'])->patch("/complete", "App\\Http\\Controllers\\TasksController@update");
+Route::middleware(['auth:sanctum'])->delete("/delete", "App\\Http\\Controllers\\TasksController@delete");
 
